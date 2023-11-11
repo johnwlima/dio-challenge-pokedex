8dd3a9e7ee8f76b765a4527990c2a7f1ec1e27fa
John Lima <jwesley1210@gmail.com>
2023-11-11 15:54:39 -0300

    feat(main.js): add dinamic pokemon list

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,43 @@ const offset = 0;
 const limit = 10;
 const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10";
 
+function convertPokemonToLi (pokemon) {
+    return `
+    
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="details">
+
+            <ol class="types">
+                <li class="type">glass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img src="assets/img/Bubassaur.webp" alt="${pokemon.name}">
+        </div>
+    </li>
+
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi (pokemon)
+        }
+
+        console.log(pokemons);
+    })
     .catch((error) => console.log(error))
-    // .finally(() =>  console.log("Requisição concluida!"))
+    .finally(() =>  console.log("Requisição concluida!"))
+
+
+
