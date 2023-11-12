0a1898b79a4219dd0a089125c8cc93a3ab37b9c4
John Lima <jwesley1210@gmail.com>
2023-11-11 23:34:53 -0300

    feat(js): add load button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,38 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0
 
-function convertPokemonToLi (pokemon) {
-    return `
-    
-    <li class="pokemon ${pokemon.mainType}">
-        <span class="number ${pokemon.mainType}">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
 
-        <div class="details">
 
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${pokemon.mainType}">${type}</li>` ).join('')}
-            </ol>
+function loadPokemonItems (offset, limit) {
 
-            <img src="${pokemon.image}" 
-                 alt="${pokemon.name}">
-        </div>
-    </li>
-
-    `
+    pokeApi.getPokemons(offset, limit).then((pokemons =[]) =>{
+        const newHtml = pokemons.map((pokemon) => `
+        
+            <li class="pokemon ${pokemon.mainType}">
+                <span class="number ${pokemon.mainType}">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="details">
+        
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${pokemon.mainType}">${type}</li>` ).join('')}
+                    </ol>
+        
+                    <img src="${pokemon.image}" 
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        
+            `).join("")
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItems(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("")
+loadMoreButton.addEventListener("click", () => {
+    offset += limit 
+    loadPokemonItems(offset, limit)
 })
