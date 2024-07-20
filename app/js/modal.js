3abf0c490403e40d667c350cba64a1928958bd8e
John Lima <jwesley1210@gmail.com>
2024-07-20 17:23:22 -0300

    add modal open and close fucntion

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,7 +1,35 @@
-const modal = document.getElementById('modal');
-const pokemonCards = document.getElementsByClassName('pokemon')
+// document.addEventListener("DOMContentLoaded", () => {
+//   const modal = document.querySelector(".popup-wrapper");
+//   const pokemonCards = document.getElementsByClassName("pokemon");
 
-pokemonCards.addEventListener('click', (e) => {
-  const clickedCard = event.target
-  console.log(clickedCard);
-})
+//   for (let i = 0; i < pokemonCards.length; i++) {
+//     pokemonCards[i].addEventListener("click", () => {
+//       console.log("pokemonCard");
+//     });
+//   }
+// });
+
+const modal = document.querySelector(".popup-wrapper")
+const closePopup = document.querySelector(".popup-close")
+const h1 = document.querySelector("h1")
+const body = document.querySelector("body")
+
+desativaModal = () => { 
+    modal.style.display ="none"
+}
+
+ativaPeloTitulo = () => {
+  h1.addEventListener("click", () => {
+    modal.style.display ="flex"
+  })
+}
+
+desativaModalNoClick = () => {
+  closePopup.addEventListener("click", () => {
+    modal.style.display ="none"
+  })
+}
+
+ativaPeloTitulo()
+desativaModal()
+desativaModalNoClick()
