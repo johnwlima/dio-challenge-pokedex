f596636601c381f351359c0c631ede1acad71a4d
John Lima <jwesley1210@gmail.com>
2024-07-20 17:28:56 -0300

    add scrollY hidden to body on modal()

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,35 +1,31 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const modal = document.querySelector(".popup-wrapper");
-//   const pokemonCards = document.getElementsByClassName("pokemon");
+function modal() {
 
-//   for (let i = 0; i < pokemonCards.length; i++) {
-//     pokemonCards[i].addEventListener("click", () => {
-//       console.log("pokemonCard");
-//     });
-//   }
-// });
+  const modal = document.querySelector(".popup-wrapper")
+  const closePopup = document.querySelector(".popup-close")
+  const h1 = document.querySelector("h1")
+  const body = document.querySelector("body")
+  
+  desativaModal = () => { 
+      modal.style.display ="none"
+  }
+  
+  ativaPeloTitulo = () => {
+    h1.addEventListener("click", () => {
+      modal.style.display ="flex"
+      body.style.overflowY = "hidden"
+    })
+  }
+  
+  desativaModalNoClick = () => {
+    closePopup.addEventListener("click", () => {
+      modal.style.display ="none"
+    })
+  }
+  
+  ativaPeloTitulo()
+  desativaModal()
+  desativaModalNoClick()
 
-const modal = document.querySelector(".popup-wrapper")
-const closePopup = document.querySelector(".popup-close")
-const h1 = document.querySelector("h1")
-const body = document.querySelector("body")
-
-desativaModal = () => { 
-    modal.style.display ="none"
 }
 
-ativaPeloTitulo = () => {
-  h1.addEventListener("click", () => {
-    modal.style.display ="flex"
-  })
-}
-
-desativaModalNoClick = () => {
-  closePopup.addEventListener("click", () => {
-    modal.style.display ="none"
-  })
-}
-
-ativaPeloTitulo()
-desativaModal()
-desativaModalNoClick()
+modal()
